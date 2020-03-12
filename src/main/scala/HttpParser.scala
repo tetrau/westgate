71b35c174fc411a9c7dd11c1410481d7b25fca98
tetrau <tetrau01@gmail.com>
2020-03-12 14:23:29 +0800

    Create transitions function for ParseRequest states.

--- src/main/scala/HttpParser.scala
+++ src/main/scala/HttpParser.scala
@@ -6,13 +6,32 @@ sealed trait HttpParsingStop
 
 object HttpParser {
   def initState = ParsingRequest(ByteString.empty)
+
+  def parseRequest(state: ParsingRequest, in: ByteString): HttpParser = {
+    val totalInput = state.parsed ++ in
+    val firstLine = totalInput.takeWhile(_ != '\n')
+    val firstLineLength = firstLine.length
+    val firstLineStr = firstLine.utf8String
+    if (firstLine.endsWith(List('\r', '\n'))) {
+      val unParsed = totalInput.drop(firstLineLength)
+      val nextState = ParsingHeader(firstLineStr, ListMap.empty, ByteString.empty, firstLine)
+      nextState.input(unParsed)
+    } else if (totalInput.length > 8192) {
+      Invalid(totalInput)
+    } else {
+      ParsingRequest(totalInput)
+    }
+  }
+
 }
 
 sealed class HttpParser {
+
   def input(in: ByteString): HttpParser = this match {
     case _: Passthrough => Passthrough(in)
     case _: Invalid => Passthrough(in)
     case _: Finished => Passthrough(in)
+    case s: ParsingRequest => HttpParser.parseRequest(s, in)
   }
 }
 
