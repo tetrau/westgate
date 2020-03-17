b785494802ef4b6acfe183a1c5b63c80044e5c39
tetrau <tetrau01@gmail.com>
2020-03-17 16:08:14 +0800

    Split WestGate to separated classes.

--- src/main/scala/WestGate.scala
+++ src/main/scala/WestGate.scala
@@ -9,18 +9,21 @@ import akka.util.ByteString
 import akka.stream.scaladsl.Tcp._
 import com.typesafe.config.ConfigFactory
 
+object Setting {
+  def fromFile(configFile: File): Setting = {
+    new Setting(scala.io.Source.fromFile(configFile).getLines.mkString)
+  }
 
-class Setting(val configPath: String) {
+  def fromResource(resource: String): Setting = {
+    new Setting(scala.io.Source.fromResource(resource).getLines.mkString)
+  }
+}
+
+class Setting(configString: String) {
 
   import scala.jdk.CollectionConverters._
 
-  private val config = ConfigFactory.parseFile(new File(configPath))
-
   case class RoutingRule(headerField: String, headerValue: String, forwardTo: InetSocketAddress)
-
-  val bind = new InetSocketAddress(config.getString("bind.host"), config.getInt("bind.port"))
-  val default = new InetSocketAddress(config.getString("default.host"), config.getInt("default.port"))
-  val timeout: Long = if (config.hasPath("timeout")) config.getLong("timeout") else 15L
 
   class RoutingTable(val rules: Seq[RoutingRule]) {
     def route(headers: Seq[(String, String)]): InetSocketAddress = {
@@ -35,62 +38,79 @@ class Setting(val configPath: String) {
     }
   }
 
-  println()
+  private val config = ConfigFactory.parseString(configString)
+  val bind = new InetSocketAddress(config.getString("bind.host"), config.getInt("bind.port"))
+  val default = new InetSocketAddress(config.getString("default.host"), config.getInt("default.port"))
+  val timeout: Long = if (config.hasPath("timeout")) config.getLong("timeout") else 15L
   val forward: RoutingTable = new RoutingTable(config.getConfigList("forward").asScala.map(
     c => RoutingRule(c.getString("headerField"), c.getString("headerValue"),
       new InetSocketAddress(c.getString("host"), c.getInt("port")))
   ).toList)
 }
 
-object WestGate extends App {
-  val setting = new Setting(args(0))
-  implicit val system: akka.actor.ActorSystem = ActorSystem("WestGate")
-  implicit val ec: ExecutionContext = system.dispatcher
-  val httpParseFlow = Flow[ByteString]
+class WestGate(setting: Setting) {
+  val httpParseFlow: Flow[ByteString, HttpParser, akka.NotUsed] = Flow[ByteString]
     .scan[HttpParser](HttpParser.initState)((s, b) => s.input(b))
-  val httpParseTimeoutFlow = Flow[HttpParser].map(Left(_))
-    .keepAlive(FiniteDuration(setting.timeout, "s"), () => Right(new TimeoutException("HTTP parse timeout")))
-    .scan[HttpParser](HttpParser.initState)(
-    (state, nextWrappedState) => nextWrappedState match {
-      case Left(s) => s
-      case Right(_) =>
-        state match {
-          case s: HttpParsingStop => s
-          case ParsingRequest(parsed) => Invalid(parsed)
-          case ParsingHeader(_, _, _, parsed) => Invalid(parsed)
-        }
+  val injectTimeoutFlow: Flow[HttpParser, Either[HttpParser, TimeoutException], akka.NotUsed] =
+    Flow[HttpParser].map[Either[HttpParser, TimeoutException]](Left(_))
+      .keepAlive(FiniteDuration(setting.timeout, "s"), () => Right(new TimeoutException("HTTP parse timeout")))
+  val httpParseTimeoutFlow: Flow[Either[HttpParser, TimeoutException], HttpParser, akka.NotUsed] =
+    Flow[Either[HttpParser, TimeoutException]].scan[HttpParser](HttpParser.initState)(
+      (state, nextWrappedState) => nextWrappedState match {
+        case Left(s) => s
+        case Right(_) =>
+          state match {
+            case s: HttpParsingStop => s
+            case ParsingRequest(parsed) => Invalid(parsed)
+            case ParsingHeader(_, _, _, parsed) => Invalid(parsed)
+          }
+      })
+  val httpParserResultFlow: Flow[HttpParser, HttpParsingStop, akka.NotUsed] =
+    Flow[HttpParser].mapConcat[HttpParsingStop]({
+      case s: HttpParsingStop => List(s)
+      case _ => List.empty
     })
-  val httpParserResultFlow = Flow[HttpParser].mapConcat[HttpParsingStop]({
-    case s: HttpParsingStop => List(s)
-    case _ => List.empty
-  })
-  val httpParseResultDecodeFlow = Flow[HttpParsingStop].map(_.data)
-  val connections: Source[IncomingConnection, Future[ServerBinding]] =
-    Tcp().bind(setting.bind.getHostString, setting.bind.getPort)
-  connections.runForeach { connection =>
-    println(s"New connection from: ${connection.remoteAddress}")
-    val forwardFlow = Flow[HttpParsingStop].prefixAndTail(1).flatMapConcat({ case (first, rest) =>
-      val parseResult = first(0)
-      val forwardTarget = parseResult match {
-        case r: Finished =>
-          setting.forward.route(r.header)
-        case _ => setting.default
-      }
-      println(s"Connection from ${connection.remoteAddress} forward to $forwardTarget")
+  val httpParseResultDecodeFlow: Flow[HttpParsingStop, ByteString, akka.NotUsed] =
+    Flow[HttpParsingStop].map(_.data)
 
-      def forwardConnection =
-        Try(Tcp().outgoingConnection(forwardTarget.getHostName, forwardTarget.getPort)) match {
-          case Success(x) => x
-          case Failure(exception) =>
-            println(s"[${connection.remoteAddress}]: $exception")
-            Flow[ByteString].take(0)
+  def run(): Unit = {
+    implicit val system: akka.actor.ActorSystem = ActorSystem("WestGate")
+    implicit val ec: ExecutionContext = system.dispatcher
+    val connections: Source[IncomingConnection, Future[ServerBinding]] =
+      Tcp().bind(setting.bind.getHostString, setting.bind.getPort)
+    connections.runForeach { connection =>
+      println(s"New connection from: ${connection.remoteAddress}")
+      val forwardFlow = Flow[HttpParsingStop].prefixAndTail(1).flatMapConcat({ case (first, rest) =>
+        val parseResult = first(0)
+        val forwardTarget = parseResult match {
+          case r: Finished =>
+            setting.forward.route(r.header)
+          case _ => setting.default
         }
+        println(s"Connection from ${connection.remoteAddress} forward to $forwardTarget")
 
-      Source(first).concat(rest).via(httpParseResultDecodeFlow).via(forwardConnection)
-    })
-    connection.handleWith(httpParseFlow.via(httpParseTimeoutFlow).via(httpParserResultFlow).via(forwardFlow))
-  } onComplete (cr => system.terminate().onComplete(_ => cr match {
-    case Failure(_) => sys.exit(1)
-    case _ => sys.exit(0)
-  }))
+        def forwardConnection =
+          Try(Tcp().outgoingConnection(forwardTarget.getHostName, forwardTarget.getPort)) match {
+            case Success(x) => x
+            case Failure(exception) =>
+              println(s"[${connection.remoteAddress}]: $exception")
+              Flow[ByteString].take(0)
+          }
+
+        Source(first).concat(rest).via(httpParseResultDecodeFlow).via(forwardConnection)
+      })
+      connection.handleWith(httpParseFlow.via(injectTimeoutFlow).via(httpParseTimeoutFlow).via(httpParserResultFlow).via(forwardFlow))
+    } onComplete (cr => system.terminate().onComplete(_ => cr match {
+      case Failure(_) => sys.exit(1)
+      case _ => sys.exit(0)
+    }))
+  }
+}
+
+object WestGate {
+  def main(args: Array[String]): Unit = {
+    val setting = Setting.fromFile(new File(args(0)))
+    val westgate = new WestGate(setting)
+    westgate.run()
+  }
 }
